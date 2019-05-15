538fc363bdd2dcd4f4fbf32393a2691efdb3b88e
LiuShii <1164818081@qq.com>
2019-05-15 21:56:22 +0800

    第二种方法

--- src/com/company/demo.java
+++ src/com/company/demo.java
@@ -1,20 +1,36 @@
 package com.company;
 
+import javax.annotation.processing.Messager;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class MyException extends Exception{
+    MyException(String messager){
+        super(messager);
+    }
+}
 public class demo {
-    public static int parseInt(String str){
+    public static int parseInt(String str) throws MyException {
         int a = 0;
-        try{
-             a = Integer.parseInt(str);
-        }catch (Exception e){
-            System.out.println(e);
+        if(!isnum(str)){
+            throw new MyException("不是数字");
         }
+        a = Integer.parseInt(str);
+
+
+
         return a;
 
     }
+    static Boolean isnum(String str){
+        Pattern pattern = Pattern.compile("\\d*");
+        Matcher matcher = pattern.matcher(str);
+        return matcher.matches();
+    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyException{
         String string = "123567";
-       String string2 = "sda55dsad54535";
+        String string2 = "sda55dsad54535";
 
         System.out.println(parseInt(string));
         System.out.println(parseInt(string2));
